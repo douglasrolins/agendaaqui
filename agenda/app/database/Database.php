b804a22e0dcaf6b5c676f9c0ae6a95c41e1f50d9
douglasrolins <douglasrolins@gmail.com>
2024-12-11 22:01:28 -0300

    Criação do método connect e closeConnection

--- agenda/app/database/Database.php
+++ agenda/app/database/Database.php
@@ -9,7 +9,7 @@ class Database
     private $user;
     private $password;
     private $dbname;
-    private $conn;
+    private $connection;
 
     // Método construtor
 
@@ -20,10 +20,27 @@ class Database
         $this->password = DB_PASSWORD;
         $this->dbname = DB_NAME;  
     }
-    
+
+
+    // Método para criar uma conexão com o banco de dados
+    public function connect()
+    {
+        $this->connection = new mysqli($this->servername,$this->user,$this->password,$this->dbname);
+
+        return $this->connection;
+    }
+
+    // Método para fechar uma conexão do banco de dados
+    public function closeConnection()
+    {
+        if ($this->connection){
+            $this->connection->close();
+        }
+
+    }
+
+
+
 }
-
-
-
 
 ?>
